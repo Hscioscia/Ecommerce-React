c3108308ac48f404cbec51466057afae117d39bf
hscioscia <hernan.scioscia@gmail.com>
2022-06-08 19:37:59 -0300

    React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navigation />
         <h1>Tienda Online DeGamers</h1>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+import './Navigation.css';
+
+const Navigation = () => {
+return(
+<nav>
+<ul>
+  <li><a class="active" href="#home">Inicio</a></li>
+  <li><a href="#products">Productos</a></li>
+  <li><a href="#gaming">Zona Gaming</a></li>
+  <li><a href="#contact">Contacto</a></li>
+</ul>
+</nav>
+)
+};
+
+export default Navigation;
